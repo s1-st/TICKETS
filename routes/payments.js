de2bd9fb8e6f89d8e704ce93a7a65dce2f732d55
s1-st <mwathageoffrey0@gmail.com>
2026-06-22 08:32:53 +0300

    Create payments.js

---
+++ routes/payments.js
@@ -0,0 +1,220 @@
+const express =
+require("express");
+
+const Payment =
+require("../models/Payment");
+
+const Ticket =
+require("../models/Ticket");
+
+const Event =
+require("../models/Event");
+
+const { v4: uuid } =
+require("uuid");
+
+const QRCode =
+require("qrcode");
+
+const { stkPush } =
+require("../utils/mpesa");
+
+const router =
+express.Router();
+
+/* =====================
+START PAYMENT
+===================== */
+
+router.post(
+"/pay",
+
+async(req,res)=>{
+
+try{
+
+const {
+
+email,
+phone,
+eventId
+
+}
+=
+req.body;
+
+const event =
+await Event
+.findById(
+eventId
+);
+
+if(
+!event
+){
+
+return res
+.status(404)
+.json({
+
+message:
+"Event not found"
+
+});
+
+}
+
+const pay =
+await stkPush({
+
+phone,
+
+amount:
+event.price,
+
+eventId
+
+});
+
+await Payment
+.create({
+
+email,
+
+phone,
+
+amount:
+event.price,
+
+eventId,
+
+checkoutId:
+pay
+.CheckoutRequestID
+
+});
+
+res.json({
+
+success:true,
+
+message:
+"Check M-Pesa",
+
+checkout:
+pay
+.CheckoutRequestID
+
+});
+
+}catch(e){
+
+res
+.status(500)
+.json({
+
+error:
+e.message
+
+});
+
+}
+
+}
+);
+
+/* =====================
+CALLBACK
+===================== */
+
+router.post(
+"/callback",
+
+async(req,res)=>{
+
+try{
+
+const data =
+req.body;
+
+const callback =
+
+data
+.Body
+.stkCallback;
+
+if(
+callback
+.ResultCode === 0
+){
+
+const checkout =
+
+callback
+.CheckoutRequestID;
+
+const payment =
+
+await Payment
+.findOne({
+
+checkoutId:
+checkout
+
+});
+
+if(
+payment
+){
+
+const qr =
+
+await QRCode
+.toDataURL(
+
+uuid()
+
+);
+
+await Ticket
+.create({
+
+email:
+payment.email,
+
+eventId:
+payment.eventId,
+
+ticketCode:
+uuid(),
+
+qr
+
+});
+
+payment.status =
+"paid";
+
+await payment.save();
+
+}
+
+}
+
+res.sendStatus(
+200
+);
+
+}catch{
+
+res.sendStatus(
+500
+);
+
+}
+
+}
+);
+
+module.exports =
+router;
